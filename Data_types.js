470768b40cbac21b8d46958eaf0d36bc586fe0e5
ercx <ercx@fresnou.org>
2023-02-06 09:51:15 -0800

    initial commit

---
+++ Data_types.js
@@ -0,0 +1,33 @@
+console.log(19);
+//Is this a comment?
+
+//Numbers
+//Integers
+console.log(10);
+console.log(21);
+//Floating Point Number
+console.log(5.6);
+console.log(42.87);
+
+//Strings
+//Characters, letters, symbols, spaces
+console.log("haha yes");
+console.log("haha yes.");
+console.log("haha yes 333");
+
+//Arithmetic Operator (Math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives us *
+console.log(14/2);
+console.log(14%2); //% is called MOD and means remainder after dividing
+console.log(15%10);
+
+//Concatenation: combine strings
+console.log("My name"+"is name");
+console.log('My'+''+'age'+''+'is'+''+'15'+'.');
+console.log("Your age is\n21");  // \n means new line
+
+//Template literal - plug in a calculated value
+//use backticks ``next to the 1
+console.log(`15 divided by 10 = ${15/10} axcvzxcvx`);
